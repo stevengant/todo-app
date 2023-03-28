a3276247b8e196492cd923ba9d4300c3042d397e
steven gant <sgant39@gmail.com>
2023-03-28 01:28:08 +0000

    lab31

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 import Todo from './Components/Todo';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Todo />
+      <>
+        <Header />
+        <Todo />
+        <Footer />
+      </>
     );
   }
 }
